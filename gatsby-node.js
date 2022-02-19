0dcacaa4d62ef24bf61978dea350bb1bc23b3aa9
jgonz19 <90273230+jgonz19@users.noreply.github.com>
2022-02-19 12:42:32 +0100

    adding new blog to github for gatsby setup

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,4 @@
-exports.createPages = async ({ actions }) => {
+/* exports.createPages = async ({ actions }) => {
   const { createPage } = actions
   createPage({
     path: "/using-dsg",
@@ -6,4 +6,48 @@ exports.createPages = async ({ actions }) => {
     context: {},
     defer: true,
   })
+}
+ */
+const { createFilePath } = require(`gatsby-source-filesystem`);
+const path = require(`path`);
+exports.onCreateNode = ({node, getNode, actions}) =>{
+  const {createNodeField} = actions
+  if(node.internal.type === `MarkdownRemark`){
+    const slug = createFilePath({node, getNode})
+    createNodeField({
+      node, 
+      name: `slug`,
+      value: slug
+    })
+  }
+}
+
+exports.createPages = ({ graphql, actions }) =>{
+  const {createPage} = actions
+  return graphql(`
+  {
+    allMarkdownRemark {
+      edges {
+        node {
+          fields {
+            slug
+          }
+        }
+      }
+      totalCount
+    }
+  }
+  
+  `).then(result=>{
+    result.data.allMarkdownRemark.edges.forEach(({node})=>{
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          slug: node.fields.slug
+        }
+      })
+    })
+  })
+ 
 }
